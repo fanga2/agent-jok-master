27a80a0f34b8d69dfaf1c9680f7ca1f732b4784b
fanga2 <fanga2>
2020-12-13 15:28:55 -0500

    added image recognition files

---
+++ extract-image.js
@@ -0,0 +1,39 @@
+const {classifyImage} = require('./imageRecognition.js');
+const {setLogLevel, logExpression} = require('@cisl/zepto-logger');
+let logLevel = 1; // default log level
+setLogLevel(logLevel);
+
+function interpretImage(watsonResponse) {
+  logExpression("In interpretImage, watsonResponse is: ", 2);
+  logExpression(watsonResponse, 2);
+  let bestClass = {};
+  let bestScore = 0.0;
+  for(i = 0; i < watsonResponse.classes.length; i++) {
+  	if(watsonResponse.classes[i].score > bestScore) {
+  	  bestClass = watsonResponse.classes[i];
+  	  bestScore = watsonResponse.classes[i].score;
+  	}
+  }
+
+  let cmd = {};
+  if(bestClass.class == "cake" && bestScore > 0.2){
+  	cmd = {
+  	  type: "cake"
+  	};
+  }else if(bestClass.class == "pancake" && bestScore > 0.2){
+  	cmd = {
+  	  type: "pancake"
+  	};
+  }else{
+  	cmd = {
+  	  type: "unknown"
+  	};
+  }
+  logExpression("Returning from interpretImage with cmd: ", 2);
+  logExpression(cmd, 2);
+  return cmd;
+}
+
+exports = module.exports = {
+  interpretImage
+};
---
+++ imageRecognition.js
@@ -0,0 +1,49 @@
+const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+const assistantParams = require('./imageParams.json');
+const { logExpression } = require('@cisl/zepto-logger');
+
+const visualRecognition = new VisualRecognitionV3({
+  version: '{version}',
+  authenticator: new IamAuthenticator({
+    apikey: '{apikey}',
+  }),
+  serviceUrl: '{url}',
+  headers: {
+    'X-Watson-Learning-Opt-Out': 'true'
+  }
+});
+
+function classifyImage(input) {
+  logExpression("Entered classifyImage with input: ", 2);
+  logExpression(input, 2);
+  let text = null;
+  text = input.replace(/[\t\r\n]+/g,"").trim();
+  let classifyParams = {
+  	url: input,
+  	owners: ['me'],
+  	threshold: 0.0,
+  };
+  logExpression("classifyParams: ", 2);
+  logExpression(classifyParams, 2);
+
+  return visualRecognition.classify(classifyParams)
+  .then(response => {
+  	return translateWatsonResponse(response);
+  })
+  .catch(err => {
+  	logExpression("Error classifying image: ", 1);
+  	logExpression(err, 1);
+  });
+}
+
+function translateWatsonResponse(response) {
+  let result = response.result;
+  let images = result.images[0] || {};
+  let output = images.classifiers[0] || {};
+  return output;
+}
+
+exports = module.exports = {
+  classifyImage
+};
